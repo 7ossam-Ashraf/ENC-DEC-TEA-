856ce8384f10cdf4d2852dbbfa339d509be5cea3
mm65 <33755999+mm65@users.noreply.github.com>
2021-01-10 22:47:50 +0200

    Adding The Decryption Algorithm and Data required to preform it

--- Decryption.asm
+++ Decryption.asm
@@ -18,6 +18,22 @@ LINE_1 BYTE "--------------------------------------",0
 ;-------------------------------------------------------
 ; User Prompt To Enter The String
 PROMPT1 BYTE "Enter Encrypted String: ",0
+PROMPT2 BYTE "Message = ",0
+PROMPT3 BYTE "ENC_Message = ",0
+;-------------------------------------------------------
+;-------------------------------------------------------
+; Input Message Data
+MAX = 80                          ;max chars to read
+MSG BYTE MAX+1 DUP (?)            ;room for null
+MSG_ENC DWORD MAX+1 DUP(?)        
+;-------------------------------------------------------
+DELTA DWORD 9e3779b9H
+SUM DWORD 0
+
+KEY_0 DWORD 20   ; Key
+KEY_1 DWORD 3
+KEY_2 DWORD 50
+KEY_3 DWORD 100
 
 .code 
 main PROC 
@@ -40,6 +56,56 @@ CALL DrawLine               ; Draw Line
 MOV  EDX, OFFSET MSG_ENC
 CALL StrLength
 MOV  MSG_ENC_LEN, EAX
+;-------------------------------------------------------
+;-------------------------------------------------------
+; Decrypting the Whole Message
+MOV ESI, OFFSET MSG_ENC     ; ESI is pointing at the beginig of the encrypted message
+MOV EDX, OFFSET MSG_ENC		; EDX = Begining of MSG_ENC
+ADD EDX, MSG_ENC_LEN		; EDX = MSG_ENC + 4*(Length)
+SUB EDX, 8					; EDX = MSG_ENC + 4*(Length - 2)
+MESSAGE_LOOP:
+    MOV ECX, 32 
+    MOV SUM, 0C6EF3720H
+    DECRYPTION_LOOP:
+
+        ; Calculating V[1]
+        MOV EAX, [ESI]              ; EAX = V[0]
+        SHL EAX, 4                  ; EAX = (V[0]<<4)
+        ADD EAX, KEY_2              ; EAX = (V[0]<<4 + KEY[2])
+        MOV EBX, [ESI]              ; EBX = V[0]
+        ADD EBX, SUM                ; EBX = V[0] + sum
+        XOR EAX, EBX                ; EAX = (V[0]<<4 + KEY[2]) ^ (V[0] + sum)
+        MOV EBX, [ESI]              ; EBX = V[0]
+        SHR EBX, 5                  ; EBX = V[0]>>5
+        ADD EBX, KEY_3              ; EBX = V[0]>>5 + KEY[3]
+        XOR EAX, EBX                ; EAX = (V[0]<<4 + KEY[2]) ^ (V[0] + sum) ^ (V[0]>>5 + KEY[3])
+        SUB [ESI+4], EAX            ; V[1] -= (V[0]<<4 + KEY[2]) ^ (V[0] + sum) ^ (V[0]>>5 + KEY[3])
+
+        ; Calculating V[0]
+        MOV EAX, [ESI+4]            ; EAX = V[1]
+        SHL EAX, 4                  ; EAX = (V[1]<<4)
+        ADD EAX, KEY_0              ; EAX = (V[1]<<4 + KEY[0])
+        MOV EBX, [ESI+4]            ; EBX = V[1]
+        ADD EBX, SUM                ; EBX = V[1] + sum
+        XOR EAX, EBX                ; EAX = (V[1]<<4 + KEY[0]) ^ (V[1] + sum)
+        MOV EBX, [ESI+4]            ; EBX = V[1]
+        SHR EBX, 5                  ; EBX = V[1]>>5
+        ADD EBX, KEY_1              ; EBX = V[1]>>5 + KEY[1]
+        XOR EAX, EBX                ; EAX = (V[1]<<4 + KEY[0]) ^ (V[1] + sum) ^ (V[1]>>5 + KEY[1])
+        SUB [ESI], EAX              ; V[0] -= (V[1]<<4 + KEY[0]) ^ (V[1] + sum) ^ V[1]>>5 + KEY[1])
+
+        MOV EBX, DELTA              ; EBX =  0x9e3779b9
+        SUB SUM, EBX                ; SUM -= 0x9e3779b9
+
+    LOOP DECRYPTION_LOOP
+
+	CMP ESI, EDX			; If ESI is at The End of MSG_ENC Then leave
+	JZ OUT_OF_LOOP
+    ADD ESI, 8
+
+JMP MESSAGE_LOOP
+OUT_OF_LOOP:
+;-------------------------------------------------------
 main ENDP
 DrawLine PROC 
     MOV  EDX, OFFSET LINE_1
