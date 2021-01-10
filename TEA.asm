84a9f2a58502bc7825ff54202369379aeec91cbe
HOSSAM <77072323+7ossam-Ashraf@users.noreply.github.com>
2021-01-10 18:14:44 +0200

    Update TEA.asm

--- TEA.asm
+++ TEA.asm
@@ -99,17 +99,18 @@ MOV MSG_ENC_LEN, EAX		; MSG_ENC_LEN = 4*MSG_LEN
         ADD SUM, EBX        ; SUM += 0x9e3779b9
 
         ; Calculating V[0]
-        MOV EAX, V_1        ; EAX = V[1]
-        SHL EAX, 4          ; EAX = (V[1]<<4)
-        ADD EAX, KEY_0      ; EAX = (V[1]<<4 + KEY[0])
-        MOV EBX, V_1        ; EBX = V[1]
-        ADD EBX, EDX        ; EBX = V[1] + sum
-        XOR EAX, EBX        ; EAX = (V[1]<<4 + KEY[0]) ^ (V[1] + sum)
-        MOV EBX, V_1        ; EBX = V[1]
-        SHR EBX, 5          ; EBX = V[1]>>5
-        ADD EBX, KEY_1      ; EBX = V[1]>>5 + KEY[1]
-        XOR EAX, EBX        ; EAX = (V[1]<<4 + KEY[0]) ^ (V[1] + sum) ^ (V[1]>>5 + KEY[1])
-        ADD V_0, EAX        ; V[0] += (V[1]<<4 + KEY[0]) ^ (V[1] + sum) ^ V[1]>>5 + KEY[1]
+        MOV EAX, [ESI+4]            ; EAX = V[1]
+        SHL EAX, 4                  ; EAX = (V[1]<<4)
+        ADD EAX, KEY_0              ; EAX = (V[1]<<4 + KEY[0])
+        MOV EBX, [ESI+4]            ; EBX = V[1]
+        ADD EBX, SUM                ; EBX = V[1] + sum
+        XOR EAX, EBX                ; EAX = (V[1]<<4 + KEY[0]) ^ (V[1] + sum)
+        MOV EBX, [ESI+4]            ; EBX = V[1]
+        SHR EBX, 5                  ; EBX = V[1]>>5
+        ADD EBX, KEY_1              ; EBX = V[1]>>5 + KEY[1]
+        XOR EAX, EBX                ; EAX = (V[1]<<4 + KEY[0]) ^ (V[1] + sum) ^ (V[1]>>5 + KEY[1])
+        ADD [ESI], EAX              ; V[0] += (V[1]<<4 + KEY[0]) ^ (V[1] + sum) ^ V[1]>>5 + KEY[1]
+	
         ; Calculating V[1]
         MOV EAX, V_0        ; EAX = V[0]
         SHL EAX, 4          ; EAX = (V[0]<<4)
